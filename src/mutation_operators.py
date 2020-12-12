01bfbd9e8344e050d8e466976639fb628e08e552
George Kouros <george.kouros.ece@gmail.com>
2020-12-12 17:53:04 +0100

    Add mutation strength argument

--- src/mutation_operators.py
+++ src/mutation_operators.py
@@ -2,23 +2,29 @@ import numpy as np
 from individual import Individual
 
 
-def swap_mutation(individual: Individual) -> Individual:
+def swap_mutation(individual: Individual, sigma=1) -> Individual:
     """ Performs swap mutation
 
     Two genes are randomly selected and their values are swapped.
 
     Args:
         individual (Individual): Individual that will be mutated with a mutation rate alpha.
+        sigma (int): The mutation strength
     Returns:
         individual (Individual): The mutated individual.
     """
-    i, j = np.random.choice(len(individual.route), 2)
-    individual.route[i], individual.route[j] = \
-        individual.route[j], individual.route[i]
-    return individual
+    route = individual.route
+
+    for _ in range(sigma):
+        idx1, idx2 = np.random.choice(len(route), 2)
+        route[idx1], route[idx2] = route[idx2], route[idx1]
+
+    mutated_individual = Individual(individual.distance_matrix, route)
+
+    return mutated_individual
 
 
-def inversion_mutation(individual: Individual) -> Individual:
+def inversion_mutation(individual: Individual, sigma=1) -> Individual:
     """ Performs inversion mutation
 
     A random sequence of genes is selected and the order of the genes in that
@@ -26,10 +32,16 @@ def inversion_mutation(individual: Individual) -> Individual:
 
     Args:
         individual (Individual): Individual that will be mutated with a mutation rate alpha.
+        sigma (int): The mutation strength
     Returns:
         individual (Individual): The mutated individual.
     """
-    idx1, idx2 = np.sort(np.random.choice(len(individual.route), 2))
-    individual.route[idx1:idx2] = \
-            individual.route[idx1:idx2][::-1]
-    return individual
+    route = individual.route
+
+    for _ in range(sigma):
+        idx1, idx2 = np.sort(np.random.choice(len(route), 2))
+        route[idx1:idx2] = route[idx1:idx2][::-1]
+
+    mutated_individual = Individual(individual.distance_matrix, route)
+
+    return mutated_individual
